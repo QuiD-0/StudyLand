afb7b88c05110dd68496bc766e2c4941fe7061b7
QuiD-0 <wodnd101@naver.com>
2024-11-23 23:51:42 +0900

    Time: 20 ms (37.98%) | Memory: 37.8 MB (54.48%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.kt
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.kt
@@ -1,10 +1,8 @@
 class Solution {
     fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
-        var max = candies[0]
+        val max = candies.max()
         val answer = ArrayList<Boolean>()
-        for(i in 0..candies.size-1) {
-            max = Math.max(max, candies[i])
-        }
+        
         for (i in candies){
             answer.add(i + extraCandies >= max)
         }
