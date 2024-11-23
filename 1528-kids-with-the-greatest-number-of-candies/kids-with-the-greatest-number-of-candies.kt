67fb065ffab64c4fdeb14555a2540d8b29f705af
QuiD-0 <wodnd101@naver.com>
2024-11-23 23:50:10 +0900

    Time: 19 ms (55.29%) | Memory: 38.6 MB (6.30%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.kt
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.kt
@@ -2,8 +2,8 @@ class Solution {
     fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
         val max = candies.max()
         val answer = mutableListOf<Boolean>()
-        for (i in candies){
-            answer.add(i + extraCandies >= max)
+        for(i in 0..candies.size-1) {
+            answer.add((candies[i]+extraCandies >= max))
         }
         return answer
     }
