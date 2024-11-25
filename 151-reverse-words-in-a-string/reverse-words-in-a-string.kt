3ffd336fcb2afd7484bcf721975bb2ca51fbd7a2
QuiD-0 <wodnd101@naver.com>
2024-11-25 14:40:36 +0900

    Time: 25 ms (54.90%) | Memory: 37.5 MB (83.59%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun reverseWords(s: String): String {
+        return s.split(" ")
+            .filter { it.isNotEmpty() }
+            .reversed()
+            .joinToString(" ")
+    }
+}
