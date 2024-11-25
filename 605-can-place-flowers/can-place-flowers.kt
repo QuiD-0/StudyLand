e8565fff2b725a0aa8268ee50c0efed613b67fb8
QuiD-0 <wodnd101@naver.com>
2024-11-25 11:57:59 +0900

    Time: 1 ms (100.00%) | Memory: 38.2 MB (64.12%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
+        var plants = n
+        var idx = 0
+
+        while(idx < flowerbed.size && plants > 0) {
+            if(flowerbed[idx] == 0 && ( idx == 0 || flowerbed[idx-1] == 0) && (idx == flowerbed.size - 1 || flowerbed[idx+1] == 0)) {
+                flowerbed[idx] = 1
+                idx += 2
+                plants--
+            } else {
+                idx++
+            }
+        }
+
+        return plants <= 0
+    }
+}
