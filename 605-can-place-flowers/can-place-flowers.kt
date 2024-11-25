0a9f77c5ae7127e0e45dc21fdcbf6536da4e4f42
QuiD-0 <wodnd101@naver.com>
2024-11-25 11:59:17 +0900

    Time: 1 ms (100.00%) | Memory: 37.9 MB (80.47%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.kt
+++ 605-can-place-flowers/can-place-flowers.kt
@@ -5,7 +5,6 @@ class Solution {
 
         while(idx < flowerbed.size && plants > 0) {
             if(flowerbed[idx] == 0 && ( idx == 0 || flowerbed[idx-1] == 0) && (idx == flowerbed.size - 1 || flowerbed[idx+1] == 0)) {
-                flowerbed[idx] = 1
                 idx += 2
                 plants--
             } else {
