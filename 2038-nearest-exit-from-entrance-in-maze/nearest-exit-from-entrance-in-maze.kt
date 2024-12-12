4167c8e68d42595458670d03a181b4c9ef8bf31f
QuiD-0 <wodnd101@naver.com>
2024-12-12 11:45:48 +0900

    Time: 34 ms (53.85%) | Memory: 50.7 MB (58.67%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.kt
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.kt
@@ -3,27 +3,30 @@ class Solution {
         val m = maze.size
         val n = maze[0].size
 
-        val directions = arrayOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
-        val visited = Array(m) { BooleanArray(n) }
-        val queue: Queue<State> = LinkedList()
+        val visited = Array(m) { BooleanArray(n)}
+        val queue = ArrayDeque<Node>()
+                val direction = arrayOf(
+            0 to 1,
+            0 to -1,
+            1 to 0,
+            -1 to 0
+        )
 
-        queue.add(State(entrance[0], entrance[1], 0))
+        queue.add(Node(entrance[0], entrance[1], 0))
         visited[entrance[0]][entrance[1]] = true
-
         while (queue.isNotEmpty()) {
-            val state = queue.poll()
-            val row = state.row
-            val col = state.col
-            val step = state.step
+            val node = queue.removeFirst()
+            val row = node.x
+            val col = node.y
 
             if ((row == 0 || col == 0 || row == m - 1 || col == n - 1)
                 && !(row == entrance[0] && col == entrance[1])
                 && (maze[row][col] == '.')
             ) {
-                return step
+                return node.cost
             }
 
-            for ((dx, dy) in directions) {
+            for ((dx, dy) in direction) {
                 val newX = row + dx
                 val newY = col + dy
 
@@ -32,7 +35,7 @@ class Solution {
                     && !visited[newX][newY]
                     && maze[newX][newY] == '.'
                 ) {
-                    queue.add(State(newX, newY, step + 1))
+                    queue.add(Node(newX, newY, node.cost + 1))
                     visited[newX][newY] = true
                 }
             }
@@ -41,4 +44,8 @@ class Solution {
     }
 }
 
-data class State(val row: Int, val col: Int, val step: Int)
+data class Node(
+    val x: Int,
+    val y: Int,
+    val cost: Int
+)
