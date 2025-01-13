19dc8e72f72d0468add88b623f666db43d0924c8
QuiD-0 <wodnd101@naver.com>
2025-01-13 22:45:21 +0900

    Time: 11 ms (26.55%) | Memory: 36.7 MB (23.01%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.kt
+++ 216-combination-sum-iii/combination-sum-iii.kt
@@ -1,61 +1,25 @@
 class Solution {
-    val queue = ArrayDeque<Node>()
-    val ans: MutableList<MutableList<Int>> = mutableListOf()
-
     fun combinationSum3(k: Int, n: Int): List<List<Int>> {
-        for (i in 1..9) {
-            queue.add(Node(1, mutableListOf(i)))
-        }
-        while (queue.isNotEmpty()) {
-            bfs(k, n)
-        }
-        return ans
-    }
+	val result = mutableSetOf<List<Int>>()
 
-    private fun bfs(k: Int, n: Int) {
-        val data = queue.removeFirst()
-        println(data)
-        if (data.same(k, n)) {
-            ans.add(data.list)
-        } else {
-            val start = data.list.max() + 1
-            for (i in start..9) {
-                val next = data.next(i)
-                if (next.isPossible(k, n)) {
-                    queue.add(next)
-                }
+	fun dfs(i: Int, current: List<Int>, sum: Int) {
+		if (sum == n) {
+            if (current.size == k){
+                result.add(current)
             }
-        }
-    }
+		}
+
+		for (j in i..9) {
+			if (sum + j > n) {
+				break
+			}
+
+			dfs(j + 1, current + j, sum + j)
+		}
+	}
+
+	dfs(1, emptyList(), 0)
+
+	return result.toList()
 }
-
-data class Node(
-    val level: Int,
-    val list: MutableList<Int>
-) {
-    fun next(num: Int): Node {
-        return this.copy(level = level + 1, list = list.addAndReturn(num))
-    }
-
-    fun isPossible(k: Int, n: Int): Boolean {
-        if (list.size > k) return false
-        if (list.sum() > n) return false
-        return true
-    }
-
-    fun same(k: Int, n: Int): Boolean {
-        if (list.size == k) {
-            if (list.sum() == n) {
-                return true
-            }
-        }
-        return false
-    }
-
-    private fun MutableList<Int>.addAndReturn(num: Int): MutableList<Int> {
-        val list = mutableListOf<Int>()
-        this.forEach { list.add(it) }
-        list.add(num)
-        return list
-    }
 }
